09cd537b90936951d25339150f649db773494fc3
William Fernandez <williamfernandez@Williams-MacBook-Pro.local>
2022-11-11 16:16:16 -0500

    Provide skeleton for LinkedList and Node classes

---
+++ DataStructures/LinkedList.java
@@ -0,0 +1,49 @@
+public class LinkedList {
+    Node head;  
+
+    // Empty Constructor
+    LinkedList() {
+    }
+
+    LinkedList(Node head) {
+        this.head = head;
+    }
+
+    public class Node {
+        Node next;
+        int data;
+
+        // Empty constructor
+        Node() {
+
+        }
+
+        Node(int data) {
+            this.data = data;
+        }
+
+
+    }
+    
+    // Appends a node to the end of the list
+    public void appendNode(Node node) {
+
+    }
+
+    // Deletes a node with the matching value
+    public void deleteNode(int value) {
+
+    }
+
+    // Prints all values of nodes in the LinkedList
+    public void printLinkedList() {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
+
+
