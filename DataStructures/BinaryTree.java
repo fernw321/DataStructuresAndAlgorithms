367a773317f9042d0b0ee4502dada075fd91cdff
William Fernandez <williamfernandez@Williams-MacBook-Pro.local>
2022-11-14 12:39:34 -0500

    Add a few methods to the BinaryTree class

--- DataStructures/BinaryTree.java
+++ DataStructures/BinaryTree.java
@@ -1,33 +1,40 @@
-public class BinaryTree {
-    Node root;
+class Node {
+    int value;
+    Node left;
+    Node right;
 
-    class Node {
-        int value;
-        Node left;
-        Node right;
-
-        // Constructor for node
-        Node(int value) {
-            this.value= value;
-        }
-
-        public void setLeftChild(Node child) {
-            this.left = child;
-        }
-
-        public void setRightChild(Node child) {
-            this.right = child;
-        }
+    // Constructor for node
+    Node(int value) {
+        this.value= value;
     }
 
-    // Constructor for BinaryTree
-    BinaryTree(Node root) {
-        this.root = root;
+    public void setLeftChild(Node child) {
+        this.left = child;
     }
 
+    public void setRightChild(Node child) {
+        this.right = child;
+    }
+
+    // In-order traversal
+    public void inOrder(Node root) {
+        if (root == null) return;
+
+        inOrder(root.left);
+        visitNode(root);
+        inOrder(root.right);
+    }
+
+    // Print the value of the node
+    public static void visitNode(Node node) {
+        System.out.println(node.value + " ");
+    }
 
 
     public static void main(String[] args) {
         
+
     }
 }
+
+
