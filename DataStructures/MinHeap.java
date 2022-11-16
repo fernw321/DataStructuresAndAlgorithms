dc7340237c853a53de3836f64998e87dafa697a6
William Fernandez <williamfernandez@Williams-MacBook-Pro.local>
2022-11-16 12:35:37 -0500

    Insert minHeapify and insert methods

--- DataStructures/MinHeap.java
+++ DataStructures/MinHeap.java
@@ -1,3 +1,9 @@
+/* The range of leaves in a MinHeap is from N/2 to (N-1)
+The range of the internal nodes is 0 to N/2 -1
+ N is the size of the Heap array
+ */
+
+
 public class MinHeap {
     private int [] Heap;
     private int size;
@@ -43,6 +49,52 @@ public class MinHeap {
         tmp = Heap[fpos];
         Heap[fpos] = Heap[spos];
         Heap[spos] = tmp;
+    }
+
+    // Percolates each inserted elemented up until it is the min element for its substree
+    private void minHeapify(int pos) {
+        int parent = parent(pos);
+
+        if (parent >= 1) {
+            if (Heap[pos] < Heap[parent]) {
+                swap(pos,parent);
+                minHeapify(parent);
+            }
+        }
+
+        return;
+
+    }
+    // Insert elements into the Heap in left-to right order on the leafy level
+    public void insert(int element) {
+        if (size >= maxsize) {
+            return;
+        }
+        Heap[++size] = element;
+        minHeapify(size);
+    }
+    
+
+    // Print the contents of the heap. Only iterating to half the size of the heap is necessary; half of the nodes are on the last level
+    public void print() {
+        for (int i = 1; i <= size/2; i++) {
+            System.out.print(
+                " Parent: " + Heap[i]
+                + " Left child:  " + Heap[2*i]
+                + " Right child: " + Heap[2*i+1]);
+
+                System.out.println();
+        }
+    }
+
+    // Code executiong starts here
+    public static void main(String[] args) {
+        MinHeap heap = new MinHeap(7);
+        for (int i = 7; i > 0; i--) {
+            heap.insert(i);
+        }
+
+        heap.print();
     }
 
 
